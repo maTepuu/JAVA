c619cc50c395c3878f09590b1d71f2e342a349a9
maTepuu <81690595+maTepuu@users.noreply.github.com>
2021-04-22 15:40:12 +0400

    1 program

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        System.out.println(14);
+        System.out.println("\n\tjava"+11);// write your code here
+    }
+}
